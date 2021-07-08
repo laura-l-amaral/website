5e62284634a9d811334490f75ec6656b3f575c67
Breno Gomes <brenodega28@gmail.com>
2021-07-08 00:43:23 -0300

    feat: adding strapi to docker and nginx

---
+++ next/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2cf89e867eb0e4dbac43ae8b947616f7'),
+    },
+  },
+});
