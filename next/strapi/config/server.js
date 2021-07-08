c86c7834b2b2e53a7daae7fa8a254e8c081eae7e
Breno Gomes <brenodega28@gmail.com>
2021-07-08 05:50:14 -0300

    feat: correct nginx dev for strapi

--- next/strapi/config/server.js
+++ next/strapi/config/server.js
@@ -1,9 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://" + env("HOST", "0.0.0.0") + "/strapi",
   admin: {
+    admin: {
+      url: "http://" + env("HOST", "0.0.0.0") + "/admin",
+    },
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '2cf89e867eb0e4dbac43ae8b947616f7'),
+      secret: env("ADMIN_JWT_SECRET", "22c3a0b264f9cee7d3b71df56be6bace"),
     },
   },
 });
---
+++ next/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1b8446b8-bc34-4f9b-ab94-72c01bb4d623'
+};
