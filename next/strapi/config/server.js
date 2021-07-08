0affb69e44b40664634958f32e18e1a2b2f82e74
Breno Gomes <brenodega28@gmail.com>
2021-07-08 06:17:57 -0300

    fix: resetting host to 0.0.0.0

--- next/strapi/config/server.js
+++ next/strapi/config/server.js
@@ -1,15 +1,9 @@
-const host = env("HOST", "0.0.0.0");
-const httpMode =
-  ["localhost", "127.0.0.1"].indexOf(host) == -1 ? "https://" : "http://";
-
 module.exports = ({ env }) => ({
-  host: host,
+  host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: httpMode + host + "/strapi",
+  url: "http://" + env("HOST", "0.0.0.0") + "/strapi",
   admin: {
-    admin: {
-      url: httpMode + host + "/admin",
-    },
+    url: "http://" + env("HOST", "0.0.0.0") + "/admin",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "22c3a0b264f9cee7d3b71df56be6bace"),
     },
