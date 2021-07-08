e3089d855ee3ea02a31659ecca7d459847f019ea
Breno Gomes <brenodega28@gmail.com>
2021-07-08 05:56:00 -0300

    feat: adding production config for strapi

--- next/strapi/config/server.js
+++ next/strapi/config/server.js
@@ -1,10 +1,14 @@
+const host = env("HOST", "0.0.0.0");
+const httpMode =
+  ["localhost", "127.0.0.1"].indexOf(host) == -1 ? "https://" : "http://";
+
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
+  host: host,
   port: env.int("PORT", 1337),
-  url: "http://" + env("HOST", "0.0.0.0") + "/strapi",
+  url: httpMode + host + "/strapi",
   admin: {
     admin: {
-      url: "http://" + env("HOST", "0.0.0.0") + "/admin",
+      url: httpMode + host + "/admin",
     },
     auth: {
       secret: env("ADMIN_JWT_SECRET", "22c3a0b264f9cee7d3b71df56be6bace"),
